0d1e74ffdf5620cf805cee70163da38d247a051d
edurank <vinci.luiz@gmail.com>
2023-11-12 20:09:15 -0300

    Create EchoController.cs

---
+++ Controllers/EchoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fsmAPI.Controllers
+{
+    [ApiController]
+    [Route("echo")]
+    public class EchoController : ControllerBase
+    {
+        public EchoController()
+        {
+
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetUsers()
+        {
+            return Ok(DateTime.Now);
+        }
+    }
+}
